cec571dad70c3d832b53e1acd3275916ec6df76b
Guilherme Miranda <guilhermemiranda419@gmail.com>
2024-10-01 09:01:14 -0300

    criando-metodo

--- src/lista/No.java
+++ src/lista/No.java
@@ -1,13 +1,10 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package lista;
-
-/**
- *
- * @author guilh
- */
-public class No {
+public class No<Tipo> {
+    private Tipo elemento;
+    private No<Tipo> proximo;
     
+    public No(Tipo elemento){
+        this.elemento = elemento;
+        this.proximo = null;
+    }
 }
