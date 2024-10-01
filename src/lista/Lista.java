a5c2e179e19ac29ac8c9b74a2a370eb704ef7cf1
Guilherme Miranda <guilhermemiranda419@gmail.com>
2024-10-01 09:36:38 -0300

    teste

--- src/lista/Lista.java
+++ src/lista/Lista.java
@@ -5,18 +5,18 @@ public class Lista {
         System.out.println("Tamanho da Lista = " + lista.getTamanho());
         
         lista.adiciona("Claudio");
-        
-        System.out.println(lista);
-        System.out.println("Tamanho da Lista = " + lista.getTamanho());
-        System.out.println("Inicio da Lista = " + lista.getInicio().getElemento());
-        System.out.println("Fim da Lista = " + lista.getFim().getElemento());
-        
         lista.adiciona("Camila");
+        lista.adiciona("Miguel");
+        lista.adiciona("Elis");
         
         System.out.println(lista);
-        System.out.println("Tamanho da Lista = " + lista.getTamanho());
-        System.out.println("Inicio da Lista = " + lista.getInicio().getElemento());
-        System.out.println("Fim da Lista = " + lista.getFim().getElemento());
+        System.out.println("Tamanho da lista = " + lista.getTamanho());
+        System.out.println("Inicio da lista = " + lista.getInicio().getElemento());
+        System.out.println("Fim da lista = " + lista.getFim().getElemento());
+        
+        System.out.println("Elemento na posição 2 = " + lista.get(2).getElemento());
+        
+      
         
     }
 }
