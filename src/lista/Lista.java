11df0231a23d0124dcc9f7b494dea5255afb1ec6
Guilherme Miranda <guilhermemiranda419@gmail.com>
2024-10-01 09:22:06 -0300

    fila-teste

--- src/lista/Lista.java
+++ src/lista/Lista.java
@@ -1,13 +1,22 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package lista;
-
-/**
- *
- * @author guilh
- */
 public class Lista {
-    
+    public static void main(String[] args) {
+        ListaEncadeada<String> lista = new ListaEncadeada<>();
+        System.out.println("Tamanho da Lista = " + lista.getTamanho());
+        
+        lista.adiciona("Claudio");
+        
+        System.out.println(lista);
+        System.out.println("Tamanho da Lista = " + lista.getTamanho());
+        System.out.println("Inicio da Lista = " + lista.getInicio().getElemento());
+        System.out.println("Fim da Lista = " + lista.getFim().getElemento());
+        
+        lista.adiciona("Camila");
+        
+        System.out.println(lista);
+        System.out.println("Tamanho da Lista = " + lista.getTamanho());
+        System.out.println("Inicio da Lista = " + lista.getInicio().getElemento());
+        System.out.println("Fim da Lista = " + lista.getFim().getElemento());
+        
+    }
 }
--- src/lista/ListaEncadeada.java
+++ src/lista/ListaEncadeada.java
@@ -3,4 +3,57 @@ public class ListaEncadeada<Tipo> {
     private No inicio;
     private No fim;
     private int tamanho;
+
+    public ListaEncadeada() {
+        this.tamanho = 0;
+    }
+
+    public ListaEncadeada(int tamanho) {
+        this.tamanho = tamanho;
+    }
+    
+    
+
+    public No getInicio() {
+        return inicio;
+    }
+
+    public void setInicio(No inicio) {
+        this.inicio = inicio;
+    }
+
+    public No getFim() {
+        return fim;
+    }
+
+    public void setFim(No fim) {
+        this.fim = fim;
+    }
+
+    public int getTamanho() {
+        return tamanho;
+    }
+
+    public void setTamanho(int tamanho) {
+        this.tamanho = tamanho;
+    }
+
+    @Override
+    public String toString() {
+        return "ListaEncadeada{" + "inicio=" + inicio + '}';
+    }
+    
+    public void adiciona(Tipo elemento){
+        No<Tipo> celula = new No<Tipo>(elemento);
+        if (this.inicio == null && this.fim == null){
+            this.inicio  = celula;
+            this.fim = celula;
+        }else{
+            this.fim.setProximo(celula);
+            this.fim = celula;
+        }
+        this.tamanho++;
+    }
+    
+    
 }
